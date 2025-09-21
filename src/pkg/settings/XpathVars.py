481e4e3fa236274bff4eca298ac4b5225fe1a022
Ajax Lima <ajaxlima94@gmail.com>
2025-09-21 20:30:25 -0300

    fix: removendo heranca do pydantic

--- src/pkg/settings/XpathVars.py
+++ src/pkg/settings/XpathVars.py
@@ -1,7 +1,4 @@
-from pydantic_settings import BaseSettings
-
-
-class XpathSettings(BaseSettings):
+class XpathSettings:
     login_input: str = "/html/body/div[2]/form/div/div[2]/div[1]/input"
     psw_input: str = "/html/body/div[2]/form/div/div[2]/div[2]/input"
     login_button: str = "/html/body/div[2]/form/div/div[2]/div[3]/button"
@@ -9,7 +6,3 @@ class XpathSettings(BaseSettings):
     wait_to_complete: str = "/html/body/div[3]/div/div[2]/span"
     next_lesson_button: str = "/html/body/div[2]/a"
     complete_lesson_when_not_a_video: str = "/html/body/div[4]/div/div[1]/a"
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
