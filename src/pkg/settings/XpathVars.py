830b3ab277156222d3f6d0e00b80f0d1a59bac43
Ajax Lima <ajaxlima94@gmail.com>
2025-09-21 23:19:19 -0300

    chore: alteracao do xpath para o botao de avancar para a proxima aula

--- src/pkg/settings/XpathVars.py
+++ src/pkg/settings/XpathVars.py
@@ -3,6 +3,9 @@ class XpathSettings:
     psw_input: str = "/html/body/div[2]/form/div/div[2]/div[2]/input"
     login_button: str = "/html/body/div[2]/form/div/div[2]/div[3]/button"
     first_lesson_button: str = '//*[@id="tab-curriculum"]/ol/li[1]/ol/li[1]'
-    wait_to_complete: str = "/html/body/div[3]/div/div[2]/span"
-    next_lesson_button: str = "/html/body/div[2]/a"
+    wait_to_complete: str = "js-lesson-completed"
+    next_lesson_button: str = (
+        "//div[contains(@class, 'c-video-navigator--next')]/a[contains(@class, 'c-video-navigator__arrow')]"
+    )
     complete_lesson_when_not_a_video: str = "/html/body/div[4]/div/div[1]/a"
+    video: str = "iframe[src*='player.vimeo.com']"
